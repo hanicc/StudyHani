f86db7495aa495f49c68e804a96b816439c96704
hani <hani_c@naver.com>
2021-05-31 19:24:16 +0900

    로딩바 게이지

---
+++ test/js/script8.js
@@ -0,0 +1,43 @@
+$(function(){
+    
+    var pogressWarp = $('.progress-bar'),
+        pogressBar = pogressWarp.find('.bar'),
+        pogressText = pogressWarp.find('.rate'),
+        pogressRate = pogressText.attr('data-rate');
+
+    //bar animation 2.5s width바뀌는 애니메이트
+    pogressBar.animate({width:pogressRate + '%'}, 2500);
+
+    /* 첫번째 setinterval을 이용한 게이지 바 */
+    /*
+    setInterval(textAnimation, 1000/10);
+    
+    function textAnimation(){
+        var currentWidth = pogressBar.width() / pogressWarp.width() *100;
+        pogressText.text(Math.ceil(currentWidth) + '%');
+    }
+    */
+
+
+
+    /* 두번째 animate 이용한 게이지 바*/
+    /*
+    //(animation옵션을 풀때)
+    pogressBar.animate({width: '60%'}, {
+        duration: 2500,
+        easing: 'easeInOutQuint',
+        complete: function(){...}
+    });
+    */
+    
+    
+    //이게 두번째 방법
+    $({rate:0}).animate({rate:pogressRate},{
+        duration: 2500,
+        progress: function(){
+            var now = this.rate;
+            pogressText.text(Math.ceil(now)+'%')
+        }
+    });
+    
+});
