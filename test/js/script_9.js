6e3ec6f5adb1f80b37cf7fe91ec79ffac40ce89e
hani <hani_c@naver.com>
2021-06-02 22:27:58 +0900

    20210602_1차 홈페이지 종합

--- test/js/script_9.js
+++ test/js/script_9.js
@@ -1,4 +1,57 @@
 $(function(){
+    //상단 메뉴 고정
+    var $header = $('header');
+    var $services = $('.services');
+    var $counters = $('.counters');
+    var $counterData = $counters.find('h3');
+
+    var serviceExecuted = false;// 이 조건을 만들고
+    var counterExecuted = false;
+
+    $(window).scroll(function(){
+        var $currentSct = $(this).scrollTop();
+        var $offset = 300;
+
+        if($currentSct > 0) {
+            $header.addClass('sticky');
+        } else {
+            $header.removeClass('sticky');
+        }
+
+        //service-item 나타나기
+        var serviceTreshold = $services.offset().top - $offset;
+
+        if(!serviceExecuted) { //serviceExecuted false가 아니라면 if문을 실행
+            if($currentSct > serviceTreshold) {
+                $services.addClass('active');
+                serviceExecuted= true; //실행하고 나서 serviceExecuted true
+            }
+        }
+
+        //숫자 애니메이션
+        var $countersTreshold = $counters.offset().top - $offset;
+
+        if(!counterExecuted) { 
+            if($currentSct > $countersTreshold) {
+                $counterData.each(function(){
+                    var $current = $(this);// 각각의 h3
+                    var $target = $current.attr('data-rate');
+                    //animate, progress, rate
+                    $({rate: 0}).animate({rate: $target},{
+                        duration: 2500,
+                        progress:function(){
+                            var now = this.rate;
+                            $current.text(Math.ceil(now))
+                        }
+                    });
+                });
+                counterExecuted = true;
+            }
+        }
+        
+    }); //scroll event
+
     
     
+
 });
